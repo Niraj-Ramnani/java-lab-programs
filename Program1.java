a961a6cd3f6ac4db65c5143e1226d316d40f5874
Niraj-Ramnani <neerajramnani800@gmail.com>
2024-03-03 19:54:52 +0530

    added program1

---
+++ Program1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Program1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter three distinct number");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if(a>b && a>c) {
+		
+			System.out.print(a + " is gretaest");
+		}
+		else if(b>a && b>c) {
+		
+			System.out.print(b + " is gretaest");
+		}
+		else {
+			
+			System.out.print(c + " is gretaest");
+		}
+
+	}
+
+}
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # java-lab-programs
-All those programs which are asked in Arya college java lab are discussed here 
+All those programs which are asked in Arya college java lab are discussed here
+<br> 
  here solutions of java programs asked in lab are provided and this repository get update as further questions are given
+ 
  <br>
   ~ author  Niraj Ramnani 
