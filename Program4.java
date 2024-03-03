4fb5f201852a6205fcd907b68a9c409fddd93b85
Niraj-Ramnani <neerajramnani800@gmail.com>
2024-03-03 20:11:51 +0530

    Program 4 and Program5 are added

---
+++ Program4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Program4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter the range ");
+		int range = sc.nextInt();
+		for(int i = 1 ; i<=range; i++) {
+			for(int j = 1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Program5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Program5{
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter first digit ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter second digit ");
+		int num2 = sc.nextInt();
+		int sum = num1 + num2;
+		System.out.println("Sum of the two digits is " + sum);
+		
+
+	}
+
+}
