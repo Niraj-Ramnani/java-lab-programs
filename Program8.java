d6c6d7763b9c8f97a25bb59c5402a601da7e0d3e
Niraj-Ramnani <neerajramnani800@gmail.com>
2024-03-09 18:20:14 +0530

    Program 8 was added

---
+++ Program8.java
@@ -0,0 +1,25 @@
+//write a java program to check given string is palidrome or not 
+
+import java.util.Scanner;
+public class Program8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+    System.out.println("This program is solved by name");
+	System.out.print("Enter any string ");
+	String str = sc.next();
+        String temp = " ";
+    for(int i = str.length() -1; i>=0; i--){
+        temp = temp + str.charAt(i);
+    }
+    if(str.equals(temp)){
+        System.out.println("Given String is Palindrome");
+    }
+    else{
+        
+        System.out.println("Given String is not Palindrome");
+    }
+
+	}
+
+}
